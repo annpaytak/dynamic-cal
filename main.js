a17c65d82cafe321351b8209148939c3d63a4e43
Anna Paitak <apaitak@ancestry.com>
2020-04-13 16:58:00 +0300

    put created

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const fetchApi = "https://jsonplaceholder.typicode.com/todos";
 
 let todos = { items: null };
+let userId;
 
 const root = document.getElementById("root");
 const list = document.createElement("ul");
@@ -12,11 +13,19 @@ function init() {
     }
 }
 
+//func fetches todos and invokes fillMock...
+async function fetchTodos() {
+    let response = await fetch(fetchApi);
+    let data = await response.json();
+    fillMock(data);
+    return data;
+}
+
 //passes passed todos to table...
 function fillMock(data) {
     todos.items = data;
+    // userId = data...userId;
     fillTable(todos.items);
-    console.log(todos);
 }
 
 const setAttributes = (el, attrs) => {
@@ -27,20 +36,24 @@ const setAttributes = (el, attrs) => {
 
 //func creates todos table and fills it...
 const fillTable = (data) => {
+    // if(!todos.items) return;
     list.innerHTML = "";
     for (let i = 0; i < data.length; i++) {
         let todoItem = data[i];
         let listItem = document.createElement("li");
         list.appendChild(listItem);
 
-        let editTodoItem = document.createElement("button");
-        editTodoItem.textContent = "edit";
-        editTodoItem.addEventListener("click", (e) => {
+        let editTodoItemEl = document.createElement("button");
+        editTodoItemEl.textContent = "edit";
+        editTodoItemEl.addEventListener("click", (e) => {
             console.log(todoItem);
-            redirectToNewTab(e, todoItem);
+            editTodoItem(e, todoItem);//
         });
 
         for (const prop in todoItem) {
+            if(prop === "id")
+                setAttributes(listItem, { key: todoItem[prop] });
+
             if (prop === "completed") {
                 let checkbox = document.createElement("input");
                 checkbox.id = "checkbox";
@@ -61,17 +74,79 @@ const fillTable = (data) => {
                 listItem.appendChild(titleEl);
             }
 
-            listItem.appendChild(editTodoItem);
+            listItem.appendChild(editTodoItemEl);
         }
     }
 };
 
-//func fetches todos and invokes fillMock...
-async function fetchTodos() {
-    let response = await fetch(fetchApi);
-    let data = await response.json();
-    fillMock(data);
-    return data;
+const editTodoItem = (e, { id, userId, title, completed }) => {
+
+    console.log(e.target.parentElement);//li
+    //title convert to input
+    let titleEl = e.target.parentElement.getElementsByTagName('label')[0];
+    let titleElInp = document.createElement('input');
+    setAttributes(titleElInp, { type: "text" });
+    titleEl.parentNode.replaceChild(titleElInp, titleEl);
+    titleElInp.value = title;
+
+    //checkbox remove disabled
+    let checkbox = e.target.parentElement.getElementsByClassName("checkbox")[0];
+    checkbox.removeAttribute("disabled");
+
+    // edit change to save
+    e.target.textContent = 'save';
+    e.target.addEventListener("click", ()=>{
+        //form updated todoitem
+        let postData = {
+            id: id,
+            userId: userId,
+            title: titleElInp.value,
+            completed: checkbox.checked
+        };
+        //updated todoitem post to fetchapi
+        postUpdatedItem(postData);
+    })
+
+}
+
+const postUpdatedItem = async (postData) => {
+    //data
+    try {
+        let putMethod = {
+            method: "PUT",
+            body: JSON.stringify(postData),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        }
+        //data to json
+        //put create
+        let response = await fetch(`${fetchApi}/${postData.id}`, putMethod);
+        let data = await response.json();
+
+        if(response.ok){
+            redrawUi(data);
+        } else{
+            throw Error(response.statusText);
+        }
+
+    } catch (e) {
+        console.log(e);
+    }
+
+}
+
+const redrawUi = ({ id, title, completed }) => {
+    console.log(id, title, completed);
+
+    todos.items.map(item => {
+        if(item.id === id){
+            item.title = title;
+            item.completed = completed;
+        }
+    });
+
+    fillMock(todos.items);
 }
 
 init();
